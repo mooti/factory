82a67813487b4b00ce414ae9ffb3b6667a20b87d
Ken Lalobo <lalobo@gmail.com>
2016-02-23 11:48:25 +0000

    refactor to make it easier to override the createNew function

--- src/Testable.php
+++ src/Testable.php
@@ -13,6 +13,18 @@ trait Testable
      */
     public function createNew($className)
     {
+        return $this->_createNew($className);
+    }
+
+    /**
+     * Create a new instance of a given class
+     *
+     * @param string $className The class to create
+     *
+     * @return object The new class
+     */
+    protected function _createNew($className)
+    {
         $constructArguments = func_get_args();
         array_shift($constructArguments);
         return new $className( ...$constructArguments);
