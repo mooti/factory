c17f6ea2f0b8ce91899312cb40be4cfe568e05ef
Ken Lalobo <lalobo@gmail.com>
2016-05-12 10:06:05 +0100

    better directory structure for tests

--- tests/phpunit/src/Unit/FactoryTest.php
+++ tests/phpunit/src/Unit/FactoryTest.php
@@ -1,8 +1,6 @@
 <?php
 namespace Mooti\Test\PHPUnit\Factory\Unit;
 
-require_once(__DIR__.'/../../../vendor/autoload.php');
-
 use Mooti\Test\PHPUnit\Factory\Unit\Fixture\TestClass;
 use Mooti\Factory\Factory;
 
